068889c5b1262818cba3ee29bba6fa2a7023c23d
unknown <phpcoder@inbox.lv>
2018-10-10 19:38:37 +0300

    TDDing PaymentDatesCalculator

--- src/Service/PaymentDatesCalculator.php
+++ src/Service/PaymentDatesCalculator.php
@@ -5,23 +5,106 @@ namespace App\Service;
 
 class PaymentDatesCalculator
 {
+    /**
+     * Get payment dates table for the next $monthsCount months.
+     *
+     * @param string $yearMonth The following format is expected: "2018-10"
+     * @param int $monthsCount How many upcoming months we want to get payment dates for
+     * @return array List of basic and bonuses payment dates for given months count
+     * @throws \Exception
+     */
     public function getPaymentDatesTable(string $yearMonth, int $monthsCount): array
     {
+        $table = [];
+        foreach($this->getUpcomingMonths($yearMonth, $monthsCount) as $date){
+            $table[] = [
+                'month_name' => $date->format('F'),
+                'basic_pay_date' => $this->getBasicPayDate($date)->format('Y-m-d'),
+                'bonuses_pay_date' => $this->getBonusesPayDate($date)->format('Y-m-d'),
+            ];
+        }
 
+        return $table;
     }
 
-    private function getUpcomingMonths(string $yearMonth, int $monthsCount): array
+    /**
+     * Get range/list of upcoming dates/months (in terms of \DatePeriod)
+     * by given months-count and month-of-the-year to start range right after
+     *
+     * @param string $yearMonth The following format is expected: "2018-10"
+     *                          So the returned range for this example will begin right after
+     *                          month 10 and will start with month 11
+     * @param int $monthsCount Range size. Specifies how many future dates we want in our range.
+     * @return \DatePeriod Represents a date period. A date period allows iteration over a set of
+     *                     dates and times, recurring at regular intervals, over a given period.
+     * @throws \Exception
+     */
+    private function getUpcomingMonths(string $yearMonth, int $monthsCount): \DatePeriod
     {
+        $start = new \DateTime($yearMonth);
+        $start->modify('+1 month');
+        $start->modify('first day of this month');
 
+        $end = clone $start;
+        $end->modify("+$monthsCount months");
+
+        $interval = new \DateInterval('P1M');
+
+        return new \DatePeriod($start, $interval, $end);
     }
 
-    private function getBasicPayDate(string $yearMonth): \DateTime
+    /**
+     * Calculate/get basic pay date for the month retrieved from the given date
+     *
+     * Business rules to calculate basic pay date are as follows:
+     *
+     *      Basic pay is paid on the last working day of the month (Mon-Fri).
+     *      So if the last day of January is the 31st, and this is a Saturday,
+     *      the payment date is Friday the 30th. The same logic applies to Sunday.
+     *
+     * @param \DateTime $date Date to extract month to calculate-basic-pay-for from.
+     * @return \DateTime
+     */
+    private function getBasicPayDate(\DateTime $date): \DateTime
     {
+        $date = clone $date;
+        $date->modify('last day of this month');
 
+        // fix payment day according to business rules (if appropriate)
+        $saturdaySundayDayNumbers = [0,6];
+        while (in_array($date->format('w'), $saturdaySundayDayNumbers)) {
+            $date->modify('-1 day');
+        }
+
+        return $date;
     }
 
-    private function getBonusesPayDate(string $yearMonth): \DateTime
+    /**
+     * Get bonuses pay date based on month extracted from given date
+     *
+     * Business rules to calculate bonuses pay date are as follows:
+     *
+     *      On the 12th of every month bonuses are paid for the previous month, unless that day
+     *      is a weekend. In that case, they are paid the first Tuesday after the 12th
+     *
+     * @param \DateTime $date
+     * @return \DateTime
+     */
+    private function getBonusesPayDate(\DateTime $date): \DateTime
     {
+        $paymentDay = 12;
+        $date = clone $date;
+        $date->setDate($date->format('Y'), $date->format('m'), $paymentDay);
 
+        // fix payment day according to business rules (if appropriate)
+        $saturdaySundayDayNumbers = [0,6];
+        $tuesdayDayNumber = 2;
+        if (in_array($date->format('w'), $saturdaySundayDayNumbers)) {
+            while ($date->format('w') != $tuesdayDayNumber) {
+                $date->modify('+1 day');
+            }
+        }
+
+        return $date;
     }
 }
--- tests/Service/PaymentDatesCalculatorTest.php
+++ tests/Service/PaymentDatesCalculatorTest.php
@@ -7,13 +7,72 @@ use PHPUnit\Framework\TestCase;
 
 class PaymentDatesCalculatorTest extends TestCase
 {
-    public function testGetPaymentDatesTable____when_Calling_With_Valid_Parameters____Response_Array_Contains_Correct_Months_List()
+    public function testGetPaymentDatesTable____when_Calling_With_Valid_Parameters____Response_Array_Contains_Correct_Month_Names_List()
     {
         $calculator = new PaymentDatesCalculator();
         $yearMonth = "2018-10";
         $monthsCount = 3;
-        $calculator->getPaymentDatesTable($yearMonth, 3);
+        $table = $calculator->getPaymentDatesTable($yearMonth, $monthsCount);
 
+        $this->assertEquals($monthsCount, sizeof($table));
+        $this->assertArrayHasKey("month_name", $table[0]);
 
+        $monthNames = array_column($table, 'month_name');
+
+        $this->assertEquals('November', $monthNames[0]);
+        $this->assertEquals('December', $monthNames[1]);
+        $this->assertEquals('January', $monthNames[2]);
+    }
+
+    public function testGetPaymentDatesTable____when_Calling_With_Valid_Parameters____Response_Array_Contains_Correct_Basic_Pay_Dates_List()
+    {
+        $calculator = new PaymentDatesCalculator();
+        $yearMonth = "2018-10";
+        $monthsCount = 12;
+        $table = $calculator->getPaymentDatesTable($yearMonth, $monthsCount);
+
+        $this->assertEquals($monthsCount, sizeof($table));
+        $this->assertArrayHasKey("basic_pay_date", $table[0]);
+
+        $basicPayDates = array_column($table, 'basic_pay_date');
+
+        $this->assertEquals('2018-11-30', $basicPayDates[0]);
+        $this->assertEquals('2018-12-31', $basicPayDates[1]);
+        $this->assertEquals('2019-01-31', $basicPayDates[2]);
+        $this->assertEquals('2019-02-28', $basicPayDates[3]);
+        $this->assertEquals('2019-03-29', $basicPayDates[4]);
+        $this->assertEquals('2019-04-30', $basicPayDates[5]);
+        $this->assertEquals('2019-05-31', $basicPayDates[6]);
+        $this->assertEquals('2019-06-28', $basicPayDates[7]);
+        $this->assertEquals('2019-07-31', $basicPayDates[8]);
+        $this->assertEquals('2019-08-30', $basicPayDates[9]);
+        $this->assertEquals('2019-09-30', $basicPayDates[10]);
+        $this->assertEquals('2019-10-31', $basicPayDates[11]);
+    }
+
+    public function testGetPaymentDatesTable____when_Calling_With_Valid_Parameters____Response_Array_Contains_Correct_Bonuses_Pay_Dates_List()
+    {
+        $calculator = new PaymentDatesCalculator();
+        $yearMonth = "2018-10";
+        $monthsCount = 12;
+        $table = $calculator->getPaymentDatesTable($yearMonth, $monthsCount);
+
+        $this->assertEquals($monthsCount, sizeof($table));
+        $this->assertArrayHasKey("bonuses_pay_date", $table[0]);
+
+        $bonusesPayDates = array_column($table, 'bonuses_pay_date');
+
+        $this->assertEquals('2018-11-12', $bonusesPayDates[0]);
+        $this->assertEquals('2018-12-12', $bonusesPayDates[1]);
+        $this->assertEquals('2019-01-15', $bonusesPayDates[2]);
+        $this->assertEquals('2019-02-12', $bonusesPayDates[3]);
+        $this->assertEquals('2019-03-12', $bonusesPayDates[4]);
+        $this->assertEquals('2019-04-12', $bonusesPayDates[5]);
+        $this->assertEquals('2019-05-14', $bonusesPayDates[6]);
+        $this->assertEquals('2019-06-12', $bonusesPayDates[7]);
+        $this->assertEquals('2019-07-12', $bonusesPayDates[8]);
+        $this->assertEquals('2019-08-12', $bonusesPayDates[9]);
+        $this->assertEquals('2019-09-12', $bonusesPayDates[10]);
+        $this->assertEquals('2019-10-15', $bonusesPayDates[11]);
     }
 }
